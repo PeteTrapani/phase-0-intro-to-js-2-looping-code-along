32b958620265f7e986783942daf85dfeef05431d
Pete Trapani <pete.trapani91@gmail.com>
2022-08-31 08:29:21 -0500

    commit tree a4f332480222ee135dd0188c7a9c311896ddd404

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+function writeCards(names, feeling){
+    const call = []
+    for (let i = 0; i < names.length; i++) {
+        call.push(`Thank you, ${names[i]}, for the wonderful ${feeling} gift!`);
+    
+    }
+    return call;
+}
+
+function countDown(){
+    let countDown = 10
+    while (countDown > -1) {
+        console.log(countDown--)
+        }
+}
